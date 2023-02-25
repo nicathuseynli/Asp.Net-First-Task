06c53103796f5433006718d4bc0f97e8e846deb6
nicathuseynli <nijatkhh@code.edu.az>
2023-02-26 01:05:31 +0400

    aspfirsttask

---
+++ Controllers/BookController.cs
@@ -0,0 +1,62 @@
+﻿using Asp.Net_Task.Entity;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Asp.Net_Task.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BookController : ControllerBase
+    {
+        private static List<Book> _book = new List<Book>();
+        [HttpGet]
+        public IActionResult GetAll()
+        {
+            if (_book != null)
+            {
+                return Ok(_book);
+            }
+            return NotFound();
+        }
+
+        [HttpGet]
+        public Book Get(string id)
+        {
+            var get = _book.FirstOrDefault(i => i.Id == id);
+            return get;
+        }
+        [HttpPost]
+        public Book Create(Book book)
+        {
+            _book.Add(book);
+            return book;
+        }
+        [HttpPut]
+        public void Update(string id, Book bookIn)
+        {
+            var upd = _book.FirstOrDefault(b => b.Id == id);
+            if (upd != null)
+            {
+                upd.BookName = bookIn.BookName;
+                upd.Price = bookIn.Price;
+                upd.Category = bookIn.Category;
+            }
+        }
+
+        [HttpDelete]
+        public void Delete(string id)
+        {
+            var del = _book.FirstOrDefault(x => x.Id == id);
+            if (del != null)
+            {
+                _book.Remove(del);
+            }
+        }
+
+        [HttpDelete("DeleteAll")]
+        public void Delete(Book bookIn)
+        {
+            _book.Remove(bookIn);
+        }
+    }
+}
---
+++ Entity/Book.cs
@@ -0,0 +1,21 @@
+﻿namespace Asp.Net_Task.Entity
+{
+    public class Book
+    {
+        public string Id { get; set; }
+        public string BookName { get; set; }=string.Empty;
+        public decimal Price { get; set; }
+        public string Category { get; set; }=string.Empty ;
+        public string Author { get; set; }= string.Empty ;
+
+
+        public Book(string id, string bookname, decimal price, string category, string author)
+        {
+            Id = id;
+            BookName = bookname;
+            Price = price;
+            Category = category;
+            Author = author;
+        }
+    }
+}
